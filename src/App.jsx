8954ea564a3cf0e32adb0d939951cdc65d8bcec7
CihanHayta <cihanhayta8080@gmail.com>
2025-12-18 21:52:58 +0300

    Projeyi güncelledim: tema, todo ve localStorage düzeltmeleri

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,134 @@
 import "./App.css";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Card from "./component/Card";
 
 function App() {
   const inputRef = useRef();
-  const [value, setValue] = useState([]);
+
+  //LocalStogre siralama onemli yokse veri gelmez ust satirda kaydet orta satirda cagir son satirda 
+  //useeffect ile value izle ama statin altinda yap ki gelen veri once state sonra useffect ulassin
+
+  const LocalSaveTodo = (value) => {
+    localStorage.setItem("value", JSON.stringify(value));
+  };
+
+  const getLocalTodo = () => {
+    const data = localStorage.getItem("value");
+    return data ? JSON.parse(data) : [];
+  };
+
+    const [value, setValue] = useState(() => getLocalTodo());
+
+  useEffect(() => {
+    LocalSaveTodo(value);
+  }, [value]);
+
+
+  //? default olarak active cizecek map
+  const [filter, setFilter] = useState("all");
+
+  //!Theme local cagir
+  const getTheme = () => {
+    return localStorage.getItem("theme" || "dark");
+  };
+  const [theme, setTheme] = useState(getTheme);
+  //!Theme local kaydet theme degisir degismez []
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  //! local degistike anlik calisir
+
+  const toogleTema = () => {
+    setTheme((tema) => (tema === "dark" ? "light" : "dark"));
+  };
 
   const handleEnter = (e) => {
     if (e.key === "Enter") {
       const text = inputRef.current.value.trim();
 
-      if(!text) return;
+      if (!text) return;
       // map dizi doner o yuzden diziye cevridir sperad ile
-      setValue((pre)=>[
+      setValue((pre) => [
         ...pre,
         {
-          id:crypto.randomUUID(),
-          text:text
-        }
+          id: crypto.randomUUID(),
+          text: text,
+          completed: false,
+        },
       ]);
       //input u temizler
-      inputRef.current.value="";
+      inputRef.current.value = "";
     }
   };
 
+  //!Confirm ekle delete icon // clear completed // thema ekle // localstroge(thema,todos)
 
+  ///? Prop yollanacak fonksiyonlar
 
-  
+  const Clearcompleted = () => {
+    setValue([]);
+  };
 
-  const handleDelete=(id)=>{
-      setValue(prev=>prev.filter(item=> item.id !== id));
-      console.log("handle delete tekiktlendi",id);
-      
-  }
+  const handleDelete = (id) => {
+    let dogruMu = confirm("Silmek istediginiz emin misiniz?");
 
+    if (dogruMu !== false) {
+      setValue((prev) => prev.filter((item) => item.id !== id));
+    } else {
+      return;
+    }
+  };
 
+  //!todonun icinde statusu completed alarak guncelleyecek
 
+  const toogleCompleted = (id) => {
+    setValue((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const visibleTodo = value.filter((todo) => {
+    if (filter === "") return;
+    if (filter === "active") return !todo.completed;
+    if (filter === "completed") return todo.completed;
+    console.log(todo, "visibe");
+
+    return true;
+  });
+
+  //?
 
   return (
-    <div className="container">
+    <div className={`container ${theme === "dark" ? "dark" : "light"}`}>
       <p className="title">todos</p>
+
+      {/* <img
+        onClick={() => {
+          onToggle(todo.id)
+        }}
+        className="circile-icons"
+        src={ 
+          todo.completed ? "/assets/empty.png" :"/assets/circle.png"
+        }
+        alt=""
+         
+      /> */}
+
+      <img
+        className="tema-img"
+        onClick={toogleTema}
+        src={
+          theme === "dark" ? "/assets/dark-tema.png" : "/assets/light-tema.png"
+        }
+        alt=""
+      />
+
       <div className="input-wrapper">
         {value && (
-          <img
-            className="img-arrow"
-            src="src/assets/arrow-down-01.png"
-            alt=""
-          />
+          <img className="img-arrow" src="assets/arrow-down-01.png" alt="" />
         )}
         <input
           ref={inputRef}
@@ -58,8 +140,9 @@ function App() {
       </div>
 
       {/* eklenen valuler */}
+      {/* //? benim yaptigim yol */}
 
-      {value?.map((val,index) => (
+      {/* {value?.map((val,index) => (
        <Card
        // obje olarak yolla
         value={val}
@@ -68,8 +151,18 @@ function App() {
         handleDelete={handleDelete}
           />
        
+      ))} */}
+
+      {/* //? chatin yaptigi yol */}
+
+      {visibleTodo.map((todo) => (
+        <Card
+          key={todo.id}
+          todo={todo}
+          onToggle={toogleCompleted}
+          handleDelete={handleDelete}
+        />
       ))}
-      
 
       {/* footer  */}
 
@@ -77,11 +170,31 @@ function App() {
         <div className="input-down">
           <p> {value.length} item left</p>
           <div className="input-down-inner">
-            <a href="#">All</a>
-            <a href="#">Active</a>
-            <a href="#">Completed</a>
+            <a
+              href="#"
+              style={{ color: theme === "dark" ? "white" : "black" }}
+              onClick={() => setFilter("all")}
+            >
+              All
+            </a>
+            <a
+              href="#"
+              style={{ color: theme === "dark" ? "white" : "black" }}
+              onClick={() => setFilter("active")}
+            >
+              Active
+            </a>
+            <a
+              href="#"
+              style={{ color: theme === "dark" ? "white" : "black" }}
+              onClick={() => setFilter("completed")}
+            >
+              Completed
+            </a>
           </div>
-          <a href="#">Clear completed</a>
+          <a href="#" onClick={Clearcompleted}>
+            Clear completed
+          </a>
         </div>
       )}
     </div>
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,31 +1,36 @@
 import { useState } from "react";
 import "./Card-styles.css";
 
-export default function Card({ value, handleDelete }) {
-  const [isClick, SetisClick] = useState(false);
-  const [completed, SetCompleted] = useState([]);
-  const [active, SetActive] = useState([]);
+export default function Card({ todo,onToggle, handleDelete }) {
+  // const [isClick, SetisClick] = useState(false);
+  // const [completed, SetCompleted] = useState([]);
+  // const [active, SetActive] = useState([]);
 
   //! filtered active all vb click olamsina gore todoyu getir
 
-  const handleActive = (item) => {
-    SetActive((prev) => [...prev, item]);
-  };
-  console.log(active, "active state");
+  // const handleActive = (item) => {
+  //   SetActive((prev) => [...prev, item]);
+  // };
+  // console.log(active, "active state");
 
-  const handleComplete = (item) => {
-    SetCompleted((prev) => [...prev, item]);
-  };
+  // const handleComplete = (item) => {
+  //   SetCompleted((prev) => [...prev, item]);
+  // };
 
-  console.log(completed, "compltered state");
+  // console.log(completed, "compltered state");
 
   const handleCLick = () => {
     SetisClick((a) => !a);
   };
 
+  console.log(todo);
+  
+
   return (
     <div className="Card-Container">
-      {isClick ? (
+      {
+      //? benim yaptigim yol
+      /* {isClick ? (
         <img
           onClick={() => {
             handleCLick();
@@ -45,18 +50,33 @@ export default function Card({ value, handleDelete }) {
           src="/assets/circle.png"
           alt=""
         />
-      )}
+      )} */
+      //? chat in yaptigi yol
+      <img
+        onClick={() => {
+          onToggle(todo.id)
+        }}
+        className="circile-icons"
+        src={ 
+          todo.completed ? "/assets/empty.png" :"/assets/circle.png"
+        }
+        alt=""
+         
+      />
+      
+      
+      }
 
       {/* sorun {value } vermek direk obejyi renden edemezsin o yuzden {value.item} vermelisin */}
 
-      <h4 style={{ textDecoration: isClick ? "line-through" : "none" }}>
+      <h4 style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
         
-        {value.text}
+        {todo.text}
       </h4>
 
       {/*  handle(value.id) */}
       <img
-        onClick={() => handleDelete(value.id)}
+        onClick={() => handleDelete(todo.id)}
         className="delete-icon"
         src="/assets/no.png"
         alt=""
